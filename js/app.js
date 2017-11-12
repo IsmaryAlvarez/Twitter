dfd6dcb8082b8786b4565a05bd135564f7544984
kuveee <kamilavillablanca23@gmail.com>
2017-11-12 10:51:43 -0300

    funcional y comentado

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
+// Habilitando el botón con el input en el textarea
 var textarea = document.getElementById('comment');
 textarea.addEventListener('input', function(){
 	if(textarea.length != 0 || textarea !=null || textarea.value !=''){
-		document.getElementById("btn").disabled = false;
+	document.getElementById("btn").disabled = false;
 } 		
-})
+});
 
-// Contador de caracteres
+// Agregando el Contador de caracteres
 
 var length = 140;
-var el_c = document.getElementById('count');
-el_c.innerHTML = length;
+var contador = document.getElementById('count');
+contador.innerHTML = length;
 textarea.onkeyup = function () {
   document.getElementById('count').innerHTML = (140 - this.value.length);
  if (document.getElementById('count').textContent < 0) {
@@ -26,78 +27,46 @@ textarea.onkeyup = function () {
   	return;
 }};
 
+// Función del botón de twittear
+var TwettBtn = document.getElementById('btn');
+TwettBtn.addEventListener('click', function(){
 
-var botoncito = document.getElementById('btn');
-
-
-botoncito.addEventListener('click', function(){
-	//tenemos q tomar el texto ingresado en el textarea
+	// Tomando el input ingresado en el TextArea
 	var comments = document.getElementById('comment').value;
-	
-	//limpiar el textarea
+
+	// Limpiando el textarea
 	document.getElementById('comment').value= '';
 
-	//contenedor que esta en el html
+	// Creando el contenedor que esta en el html
 	var cont = document.getElementById('cont');
 
-	//creamos el div que contiene cada comentario
+	// Creando el div que contiene cada comentario
 	var newComments = document.createElement('div');
 
-	//validar que textarea no tenga un mensaje
+	// Validando que textarea no tenga un mensaje
 	if(comments.length=== 0 || comments ==null || comments.value ===''){
 		document.getElementById("btn").disabled = true;
-		return;
-	} 
-	
-	//nodos de texto del textarea
+	}
+
+	// Creando nodos de texto del textarea
 	var textNewComment = document.createTextNode(comments);
 
-	// fecha
-	var dateSpan = document.createElement("span");
-	dateSpan.textContent = new Date();
+	// Agregando la Hora
+	var dateSpan = document.createElement("p");
+	var tiempo = new Date();
+	var hora = tiempo.getHours();
+	var minuto = tiempo.getMinutes();
+	dateSpan.textContent= hora + ':' + minuto;
 
-		 Reloj();
+	// Creando el nodo del comentario
+	var elementContainer= document.createElement('p');
+	elementContainer.appendChild(textNewComment);
 
-	
-function Reloj() {
-
-var tiempo = new Date();
-var hora = tiempo.getHours();
-var minuto = tiempo.getMinutes();
-
-setTimeout('Reloj()', 1000);
-str_hora = new String(hora);
-if (str_hora.length == 1) {
-document.getElementById('hora').innerHTML = '0' + hora +  ':';
-}else{document.getElementById('hora').innerHTML = hora +  ':';
-}
-if (str_hora.charAt(0) < 12 ){
-document.getElementById('minuto').innerHTML = minuto +  'AM';
-} else {document.getElementById('minuto').innerHTML = minuto + 'PM';
-}
-str_minuto = new String(minuto);
-if (str_minuto.length == 1) {
-document.getElementById('minuto').innerHTML = '0' + minuto;
-}
-
- 
-}
-
-
-
-
-	var contenedorElemento= document.createElement('p');
-	contenedorElemento.appendChild(textNewComment);
-
+	// Agregando los elementos al contenedor
+	newComments.appendChild(elementContainer);
 	newComments.appendChild(dateSpan);
-	newComments.appendChild(contenedorElemento);
-
 	cont.appendChild(newComments)
-	 document.getElementById('count').innerHTML = 140;
 
-
+	// Regresando el contador de caracteres a 0
+	document.getElementById('count').innerHTML = 140;
 });
-
-
-
-
